e417773136920e3fa4fa7f4e5924cb15833a787b
Arshad Pasha <arshadpashaintern@gmail.com>
2025-09-01 10:25:25 +0530

    added files

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello hi");
+        int[] arr = {5,7,3,8,7,4,3};
+        int val=0;
+        int i;
+        // for(i=0;i<10;i++)
+        // {
+        //     System.out.println(i);
+        // }
+        for(i=0;i<arr.length;i++)
+        {
+            val = arr[i] + val;
+            System.out.println(val);
+        }
+        int avg =val/arr.length;
+        System.out.println("The Avereage is " + avg);
+    }
+}
---
+++ sep1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public Hello{
+    public static void main(){
+        int arr[];
+        type[]
+    }
+}
